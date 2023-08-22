b6b0832b7f227eec7be5d00b59b73b12bff26da5
soadaa <Soadhlh@gmail.com>
2023-08-22 13:33:20 -0400

    Start of day 4, part 1
    I think I am fairly close to an answer, but I need to find the bug that is not catching true positives. My program gives me the correct answer to the sample.md but incorrect for test.md which is the first 5 lines of input.md

---
+++ day4/day4.rb
@@ -0,0 +1,47 @@
+require 'byebug'
+
+class Solver
+  def initialize filename
+    return unless File.exist?(filename)
+    @file = File.open(filename)
+    @redundant_assignment_pairs = 0
+  end
+  
+  def clean_ranges
+    first_elf_pair = []
+    second_elf_pair = []
+
+    @file.each_line do |line|
+      # split the line of the elf range of duties into 2 halves, no comma
+      first_elf_pair = line.split(',')[0]
+      second_elf_pair = line.split(',')[1].chomp
+
+      # Populate an array based on the start and end of the elf duties range
+      first_elf_start = first_elf_pair.split('-')[0].to_i
+      first_elf_end = first_elf_pair.split('-')[1].to_i
+      first_elf_ranges = Array (first_elf_start..first_elf_end)
+
+      second_elf_start = second_elf_pair.split('-')[0].to_i
+      second_elf_end = second_elf_pair.split('-')[1].to_i
+      second_elf_ranges = Array (second_elf_start..second_elf_end)
+
+      # Find the smaller Array, test if array contains other array
+      if first_elf_ranges.size <= second_elf_ranges.size
+        if (first_elf_ranges - second_elf_ranges).empty?
+          @redundant_assignment_pairs += 1
+          break
+        end
+      else
+        if (second_elf_ranges - first_elf_ranges).empty?
+          @redundant_assignment_pairs += 1
+        end
+      end
+    end
+
+    puts @redundant_assignment_pairs
+  end
+
+end
+
+a = Solver.new("test.md")
+a.clean_ranges
